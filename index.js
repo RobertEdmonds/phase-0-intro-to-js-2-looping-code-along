15bc9cebb22bc8e76d77d53692c6b292b1c7b7d8
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-14 12:34:34 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+function writeCards(names){
+    const final =[]
+    for (let i = 0; i < names.length; i++){
+        final.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return final;
+}
+
+function countDown(num){
+    let i = num;
+    while (i >= 0){
+        console.log(i--);
+    }
+    return i;
+}
+console.log(countDown(10));
